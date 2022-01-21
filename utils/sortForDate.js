b871793d2abc559b7977f7776b32d347ceab7392
Roberto Bocio Melo <elpicapica01@gmail.com>
2022-01-21 00:29:12 -0500

    cambio de ordenamiento

--- utils/sortForDate.js
+++ utils/sortForDate.js
@@ -1,1 +1,1 @@
-export const sortDate = array => array.sort((a, b) => (a.attributes.date < b.attributes.date ? 1 : -1))
+export const sortDate = array => array.sort((a, b) => (a.attributes.date > b.attributes.date ? 1 : -1))
