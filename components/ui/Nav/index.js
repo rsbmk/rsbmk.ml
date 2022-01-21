5f97d6175ff23b9f9ad1f8c3a9eaa655926174e9
Roberto Bocio Melo <elpicapica01@gmail.com>
2022-01-21 00:26:26 -0500

    cambio del home link

---
+++ components/ui/Nav/index.js
@@ -0,0 +1,21 @@
+import { Arrow } from 'components/icons/arrow'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useCallback } from 'react'
+import styles from 'styles/Home.module.css'
+
+export function Nav () {
+  const router = useRouter()
+  const handleClickBackHome = useCallback(() => router.back(), [])
+
+  return (
+    <nav className={styles.nav}>
+      <button className={styles.btnArrowBack} onClick={handleClickBackHome}>
+        <Arrow />
+      </button>
+      <Link href="/">
+        <a className={styles.homeName}>&#123; rsbmk &#125;</a>
+      </Link>
+    </nav>
+  )
+}
